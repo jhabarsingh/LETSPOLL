9ed84ab016be66f6e0c5b5c67860c07e5d2232a7
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-03 14:12:11 +0530

    changed django setting

--- live_poll/settings.py
+++ live_poll/settings.py
@@ -79,25 +79,25 @@ WSGI_APPLICATION = 'live_poll.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jethu',
-        'USER': 'postgres',
-        'PASSWORD': '9592864914',
-        'HOST': 'database-2.cu1tx0vgzwd4.us-east-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'jethu',
+#         'USER': 'postgres',
+#         'PASSWORD': '9592864914',
+#         'HOST': 'database-2.cu1tx0vgzwd4.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
