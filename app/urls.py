60105c908bf68e9cf1aae0721ae2f3edd0d7d4c2
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-03 15:48:40 +0530

    Added new url for showing poll

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from . import views
+from app import views
 
 app_name = 'poll'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register', views.register, name='register'),
     path('login', views.admin_login, name='login'),
     path('create_poll/', views.create_poll, name='create_poll'),
-    path('show_polls/', views.show_polls, name='show_polls'),
+    path('show_polls/', views.show_poll, name='show_polls'),
+    path('show_polls/<slug:username>/', views.show_polls, name='show_polls'),
     path('save_poll/', views.save_poll, name='save_poll'),
     path('polling/<uuid:id>/', views.polling, name='polling'),
     path('logout/', views.admin_logout, name='logout'),
