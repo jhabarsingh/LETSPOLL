1422ad6e320192502691af072342bd115042ed82
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-03 14:11:59 +0530

    added otp model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 import uuid
+from django.contrib.auth import get_user_model as User
 # Create your models here.
+
+class Otp(models.Model):
+	user = models.CharField(max_length=100)
+	otp = models.CharField(max_length=6)
 
 class Poll(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	question = models.CharField(max_length=100)
 
 	def __str__(self):
