ef9905baf1816f969e47c981733d574c60556fba
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-03 19:50:57 +0530

    Setting according to production

--- live_poll/settings.py
+++ live_poll/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7ef8*3cyrn2um^lx4(rh)y*r^j_6qq^%1-@_^v&u49dxcaxvz='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['letpoll.herokuapp.com', 'localhost']
 
 
 # Application definition
