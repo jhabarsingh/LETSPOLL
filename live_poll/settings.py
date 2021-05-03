76efb4a4e316b9218351bd657e90b25a06dfa0df
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-03 19:07:14 +0530

    removed s3 credentials

--- live_poll/settings.py
+++ live_poll/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
@@ -146,17 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 REAL_SITE = SITE_URL + 'polling/'
 
-# AWS_ACCESS_KEY_ID = 'AKIATEO5GSFSFSYFFRK6'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'pollingwebsite'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'static'
 
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Activate Django-Heroku.
